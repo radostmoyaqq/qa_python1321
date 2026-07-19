c417c0b847ea5ce32621321a5b578ec0fb00d3bc
Ольга <olya8575@yandex.ru>
2026-07-19 14:14:09 +0300

    Добавила решение задания

--- tests.py
+++ tests.py
@@ -1,24 +1,105 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_new_book('Pride and Prejudice and Zombies')
+        collector.add_new_book('How to Tell If Your Cat Is Plotting')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize('name', ['', 'a' * 41])
+    def test_add_new_book_invalid_name_length_not_added(self, name):
+        collector = BooksCollector()
+
+        collector.add_new_book(name)
+
+        assert name not in collector.get_books_genre()
+
+    def test_add_new_book_same_book_twice_added_once(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Dune')
+        collector.add_new_book('Dune')
+
+        assert len(collector.get_books_genre()) == 1
+
+    def test_get_book_genre_added_book_has_empty_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Dune')
+
+        assert collector.get_book_genre('Dune') == ''
+
+    def test_set_book_genre_existing_book_valid_genre_sets_genre(self):
+        collector = BooksCollector()
+        genre = collector.genre[0]
+
+        collector.add_new_book('Dune')
+        collector.set_book_genre('Dune', genre)
+
+        assert collector.get_book_genre('Dune') == genre
+
+    @pytest.mark.parametrize(
+        'book_name, genre, expected_genre',
+        [
+            ('Unknown book', 0, None),
+            ('Dune', 'Unknown genre', ''),
+        ],
+    )
+    def test_set_book_genre_invalid_book_or_genre_does_not_set_genre(
+            self, book_name, genre, expected_genre):
+        collector = BooksCollector()
+        collector.add_new_book('Dune')
+        genre = collector.genre[0] if genre == 0 else genre
+
+        collector.set_book_genre(book_name, genre)
+
+        assert collector.get_book_genre(book_name) == expected_genre
+
+    def test_get_books_with_specific_genre_returns_books_with_genre(self):
+        collector = BooksCollector()
+        target_genre = collector.genre[3]
+        other_genre = collector.genre[4]
+
+        collector.add_new_book('The Lion King')
+        collector.add_new_book('Home Alone')
+        collector.set_book_genre('The Lion King', target_genre)
+        collector.set_book_genre('Home Alone', other_genre)
+
+        assert collector.get_books_with_specific_genre(target_genre) == ['The Lion King']
+
+    def test_get_books_for_children_excludes_books_with_age_rating(self):
+        collector = BooksCollector()
+        children_genre = collector.genre[3]
+        age_rating_genre = collector.genre_age_rating[0]
+
+        collector.add_new_book('The Lion King')
+        collector.add_new_book('It')
+        collector.set_book_genre('The Lion King', children_genre)
+        collector.set_book_genre('It', age_rating_genre)
+
+        assert collector.get_books_for_children() == ['The Lion King']
+
+    def test_add_book_in_favorites_existing_book_adds_book_once(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Dune')
+        collector.add_book_in_favorites('Dune')
+        collector.add_book_in_favorites('Dune')
+
+        assert collector.get_list_of_favorites_books() == ['Dune']
+
+    def test_delete_book_from_favorites_existing_book_deletes_book(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Dune')
+        collector.add_book_in_favorites('Dune')
+        collector.delete_book_from_favorites('Dune')
+
+        assert collector.get_list_of_favorites_books() == []
